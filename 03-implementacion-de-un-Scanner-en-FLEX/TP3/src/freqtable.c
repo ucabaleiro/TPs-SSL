12821f386357c6350d347e132bd3544ee70e2568
Marco Piatti <marcopiatticastro@hotmail.com>
2021-08-02 02:03:42 -0300

    Cambios en freqtable para soportar pares lexema-linea
    Ya que son un struct de mismo tipo, reutilizamos las entradas en la tabla de frecuencia para tambien crear listas de lexemas asociados a la linea donde se hallaron

--- 03-implementacion-de-un-Scanner-en-FLEX/TP3/src/freqtable.c
+++ 03-implementacion-de-un-Scanner-en-FLEX/TP3/src/freqtable.c
@@ -1,12 +1,10 @@
 #include "freqtable.h"
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "stringso.h"
 
-typedef struct freqtable_entry {
-    char* lexeme;
-    uint16_t frequency;
-} t_freqtable_entry;
+t_freqtable_entry* freqtable_entry_create(char* lexeme);
+t_freqtable_entry* freqtable_entry_create_as_error(char* lexeme, uint32_t line);
 
 t_freqtable* freqtable_create(){
     t_freqtable* freqtable = malloc(sizeof(t_freqtable));
@@ -17,19 +15,25 @@ t_freqtable* freqtable_create(){
 
 t_freqtable_entry* freqtable_entry_create(char* lexeme){
     t_freqtable_entry* new_entry = malloc(sizeof(t_freqtable_entry));
-    new_entry->lexeme = strdup(lexeme);
+    new_entry->lexeme = string_duplicate(lexeme);
     new_entry->frequency = 1;
     return new_entry;
 }
 
-void entry_destroyer(void* element){
-    t_freqtable_entry* r_element = (t_freqtable_entry*)element;
-    free(r_element->lexeme);
-    free(r_element);
+t_freqtable_entry* freqtable_entry_create_as_error(char* lexeme, uint32_t line){
+    t_freqtable_entry* new_entry = malloc(sizeof(t_freqtable_entry));
+    new_entry->lexeme = string_duplicate(lexeme);
+    new_entry->frequency = line;
+    return new_entry;
 }
 
 void freqtable_destroy_and_destroy_elements(t_freqtable* self){
-    list_destroy_and_destroy_elements(self->elements, entry_destroyer);
+    void _entry_destroyer(void* element){
+        t_freqtable_entry* r_element = (t_freqtable_entry*)element;
+        free(r_element->lexeme);
+        free(r_element);
+    }
+    list_destroy_and_destroy_elements(self->elements, _entry_destroyer);
 	free(self);
 }
 
@@ -44,7 +48,7 @@ void freqtable_add(t_freqtable* self, char* lexeme){
     if (entry != NULL){
         entry->frequency += 1;
     }
-    else {
+    else{
         list_add(self->elements, (void*)freqtable_entry_create(lexeme));
     }
 }
@@ -58,4 +62,8 @@ void freqtable_add_alpha(t_freqtable* self, char* lexeme){
     }
     freqtable_add(self, lexeme);
     list_sort(self->elements, _is_smaller_alpha);
+}
+
+void freqtable_add_as_error(t_freqtable* self, char* lexeme, uint32_t line){
+    list_add(self->elements, (void*)freqtable_entry_create_as_error(lexeme, line));
 }
