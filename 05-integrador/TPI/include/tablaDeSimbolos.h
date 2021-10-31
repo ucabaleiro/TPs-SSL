c41a2ddd5101a12ca25a61edc383103792382c2b
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-10-31 17:10:46 -0300

    TADS
    Co-authored-by: Marco Piatti <MarcoPiatti@users.noreply.github.com>
    Co-authored-by: Dolores Lopez Joffre <dlopezjoffreutn@users.noreply.github.com>

---
+++ 05-integrador/TPI/include/tablaDeSimbolos.h
@@ -0,0 +1,38 @@
+#ifndef SYMTABLE_H_
+#define SYMTABLE_H_
+
+#include "list.h"
+#include "stringso.h"
+#include <stdbool.h>
+
+typedef struct typeInfo {
+    enum { VOID, CHAR, INT, FLOAT, DOUBLE, PTR, ARRAY } name;
+    typeInfo* next;
+} typeInfo;
+
+typedef struct symbol {
+    char* identifier;
+    enum { VAR, FUNC } kind; 
+    typeInfo type;
+    // v Usado solo para funciones v
+    t_list* params;
+    bool isDefined;
+} symbol;
+
+typedef struct symtable {
+    t_list* elems;
+} symtable;
+
+symbol *symbol_create(char *identifier);
+
+void symbol_destroy(symbol *self);
+
+symtable* symtable_create();
+
+void symtable_destroy(symtable* self);
+
+symbol* symtable_lookup(symtable* self, char* identifier);
+
+void symtable_add(symtable* self, symbol* elem);
+
+#endif SYMTABLE_H_
---
+++ 05-integrador/TPI/src/tablaDeSimbolos.c
@@ -0,0 +1,38 @@
+#include "tablaDeSimbolos.h"
+
+symtable* symtable_create(){
+    symtable* self = malloc(sizeof(symtable));
+    self->elems = list_create();
+    return self;
+}
+
+void symtable_destroy(symtable* self){
+    void _symbol_destroy(void* elem){
+        symbol_destroy((symbol*) elem);
+    };
+    list_destroy_and_destroy_elements(self->elems, _symbol_destroy);
+    free(self);
+}
+
+symbol *symbol_create(char *identifier){
+    symbol *self = malloc(sizeof(symbol));
+    self->identifier = string_duplicate(identifier);
+    self->params = list_create();
+}
+
+void symbol_destroy(symbol *self){
+    free(self->identifier);
+    list_destroy(self->params);
+    free(self);
+}
+
+symbol* symtable_lookup(symtable* self, char* identifier){
+    bool matchIdentifier(void* elem){
+        return ((symbol*)elem)->identifier == identifier;
+    };
+    return list_find(self->elems, matchIdentifier);
+}
+
+void symtable_add(symtable* self, symbol* elem){
+    list_add(self->elems, elem);
+}
